7a9aee159227223afb88e0b70a0ab4de2dea8e08
Rahul Sharma <rs.rahul.developer@gmail.com>
2026-03-23 20:47:16 +0530

    - Initial commit with the setup for solving problems

---
+++ solving-setup/main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define LOCAL
+
+int main(){
+
+    #ifdef LOCAL
+     freopen("input.txt","r",stdin);
+     freopen("output.txt","w",stdout);
+    #endif
+
+    string name;
+    cin>>name;
+    cout<<name+"sharma";
+
+}
